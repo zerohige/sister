a80c7345544ce5723c306d015c79c4f5e8ccd6f7
2201020032 - Muhamad Nur Amin <153512596+zerohige@users.noreply.github.com>
2024-12-29 16:05:01 +0700

    Add files via upload

---
+++ db/db_arsip.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Des 2024 pada 10.01
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_arsip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_nama` varchar(255) NOT NULL,
+  `admin_username` varchar(255) NOT NULL,
+  `admin_password` varchar(255) NOT NULL,
+  `admin_foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_nama`, `admin_username`, `admin_password`, `admin_foto`) VALUES
+(1, 'Muhamad Nur Amin', '2201020032', '25d9fed238404891fb5bcac2c6c7665c', '204008897_kusakabe_shinra_render_png_by_unionxw_dfs7ff2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `arsip`
+--
+
+CREATE TABLE `arsip` (
+  `arsip_id` int(11) NOT NULL,
+  `arsip_waktu_upload` datetime NOT NULL,
+  `arsip_petugas` int(11) NOT NULL,
+  `arsip_kode` varchar(255) NOT NULL,
+  `arsip_nama` varchar(255) NOT NULL,
+  `arsip_jenis` varchar(255) NOT NULL,
+  `arsip_kategori` int(11) NOT NULL,
+  `arsip_keterangan` text NOT NULL,
+  `arsip_file` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `arsip`
+--
+
+INSERT INTO `arsip` (`arsip_id`, `arsip_waktu_upload`, `arsip_petugas`, `arsip_kode`, `arsip_nama`, `arsip_jenis`, `arsip_kategori`, `arsip_keterangan`, `arsip_file`) VALUES
+(16, '2024-12-29 14:38:43', 9, '12345', 'Laporan Metnum', 'pdf', 11, 'Ini pdf laporan metnum', '1814145498_Laporan_Permintaan_Barang (18).pdf'),
+(17, '2024-12-29 14:42:03', 8, '123456', 'Laporan keuangan', 'pdf', 11, 'ini laporan keuangan PIPL', '172596274_Laporan_Permintaan_Barang.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori_nama` varchar(255) NOT NULL,
+  `kategori_keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `kategori_nama`, `kategori_keterangan`) VALUES
+(1, 'Tidak berkategori', 'Semua yang tidak memiliki kategori'),
+(4, 'Surat Izin', 'Format surat izin'),
+(7, 'Surat Kesehatan', 'Format Surat kesehatan '),
+(8, 'Surat Lampiran Skripsi', 'Surat lampiran untuk skripsi'),
+(11, 'Laporan', 'Format Laporan Tugas, Penelitian Dan lain lain.'),
+(12, 'Gambar & Video', 'Format Gambar & Video apa saja');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `petugas_id` int(11) NOT NULL,
+  `petugas_nama` varchar(255) NOT NULL,
+  `petugas_username` varchar(255) NOT NULL,
+  `petugas_password` varchar(255) NOT NULL,
+  `petugas_foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`petugas_id`, `petugas_nama`, `petugas_username`, `petugas_password`, `petugas_foto`) VALUES
+(8, 'Zayus Muazrian', '2201020002', '25d9fed238404891fb5bcac2c6c7665c', ''),
+(9, 'Rifana Bima Pradifa', '2201020026', '25d9fed238404891fb5bcac2c6c7665c', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `riwayat`
+--
+
+CREATE TABLE `riwayat` (
+  `riwayat_id` int(11) NOT NULL,
+  `riwayat_waktu` datetime NOT NULL,
+  `riwayat_user` int(11) NOT NULL,
+  `riwayat_arsip` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `riwayat`
+--
+
+INSERT INTO `riwayat` (`riwayat_id`, `riwayat_waktu`, `riwayat_user`, `riwayat_arsip`) VALUES
+(1, '2019-10-11 15:32:29', 8, 3),
+(2, '2019-10-12 17:09:31', 8, 10),
+(3, '2019-10-12 17:09:45', 8, 9),
+(4, '2019-10-12 17:09:50', 8, 8),
+(5, '2019-10-12 17:09:53', 8, 3),
+(6, '2019-10-12 17:10:07', 9, 10),
+(7, '2019-10-12 17:10:16', 9, 9),
+(8, '2019-10-12 17:10:19', 9, 8),
+(9, '2019-10-12 17:10:22', 9, 6),
+(10, '2019-10-12 17:10:26', 9, 2),
+(11, '2021-11-11 22:25:05', 13, 11),
+(12, '2023-12-01 00:39:03', 13, 12),
+(13, '2023-12-01 00:39:12', 13, 13),
+(14, '2023-12-12 22:49:06', 13, 13),
+(15, '2023-12-15 00:20:20', 14, 14),
+(16, '2023-12-15 09:02:21', 13, 13),
+(17, '2024-12-29 14:40:18', 18, 16);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_nama` varchar(100) NOT NULL,
+  `user_username` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_foto` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_nama`, `user_username`, `user_password`, `user_foto`) VALUES
+(16, 'Muhajirul Fajri', '2201020039', '25d9fed238404891fb5bcac2c6c7665c', ''),
+(17, 'Enjelita Br Ginting', '2201020035', '25d9fed238404891fb5bcac2c6c7665c', ''),
+(18, 'Abdul Arafah', '2201020058', '25d9fed238404891fb5bcac2c6c7665c', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indeks untuk tabel `arsip`
+--
+ALTER TABLE `arsip`
+  ADD PRIMARY KEY (`arsip_id`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`petugas_id`);
+
+--
+-- Indeks untuk tabel `riwayat`
+--
+ALTER TABLE `riwayat`
+  ADD PRIMARY KEY (`riwayat_id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `arsip`
+--
+ALTER TABLE `arsip`
+  MODIFY `arsip_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `petugas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `riwayat`
+--
+ALTER TABLE `riwayat`
+  MODIFY `riwayat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
